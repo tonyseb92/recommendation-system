9eb120ab90c35fc6b0b1854ab81155da08c54677
tonyseb92 <38024083+tonyseb92@users.noreply.github.com>
2018-04-03 13:20:40 -0700

    Update and rename C11UserBasedFiltering.py to UserBasedFiltering.py

--- UserBasedFiltering.py
+++ UserBasedFiltering.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Assignment 3
-"""
 
 import math
 
@@ -13,26 +10,15 @@ class UserBasedFilteringRecommender:
     
     # class variables:    
     # none
-    
-    ##################################
-    # class instantiation method - initializes instance variables
-    #
     # usersItemRatings:
     # users item ratings data is in the form of a nested dictionary:
     # at the top level, we have User Names as keys, and their Item Ratings as values;
     # and Item Ratings are themselves dictionaries with Item Names as keys, and Ratings as values
-    # Example: 
-    #     {"Angelica":{"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
-    #      "Bill":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0}}
-    #
-    # k:
-    # the number of nearest neighbors
+    # k: the number of nearest neighbors
     # defaults to 1
-    #
-    # m:
-    # the number of recommedations to return
+    # m: the number of recommedations to return
     # defaults to 10
-    #
+    
     def __init__(self, usersItemRatings, metric='pearson', k=1, m=10):
         
         # set self.usersItemRatings
@@ -52,13 +38,9 @@ class UserBasedFilteringRecommender:
             print ("    (FYI - invalid value of m (must be > 0) - defaulting to 10)")
             self.m = 10
             
-
     #################################################
     # pearson correlation similarity
-    # notation: if UserX is Angelica and UserY is Bill, then:
-    # userXItemRatings = {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0}
-    # userYItemRatings = {"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0}
-    def pearsonFn(self, userXItemRatings, userYItemRatings):
+        def pearsonFn(self, userXItemRatings, userYItemRatings):
         
         sum_xy = 0
         sum_x = 0
@@ -88,12 +70,10 @@ class UserBasedFilteringRecommender:
         else:
             return round((sum_xy - (sum_x * sum_y) / n) / denominator, 2)
             
-
     #################################################
-    # make recommendations for userX from the most similar k nearest neigibors (NNs)
+    # making recommendations for userX from the most similar k nearest neigibors (NNs)
     def recommendKNN(self, userX):
         
-        # YOUR CODE HERE
         #Creating the pearson correlation matrix
         pearsCorrtable = {}
         for name,score in self.usersItemRatings.items():
@@ -105,8 +85,6 @@ class UserBasedFilteringRecommender:
                     namedict[name2] = pears
             pearsCorrtable[name] = namedict
 
-
-        # for given userX, get the sorted list of users - by most similar to least similar        
         #converting dictionary to list for UserX and sorting
         pearsinner = pearsCorrtable[userX]
         lst = []
@@ -116,7 +94,7 @@ class UserBasedFilteringRecommender:
                 
         sortedlst = sorted(lst, key=itemgetter(1), reverse = True)
 
-        # calcualte the weighted average item recommendations for userX from userX's k NNs
+        # calculating the weighted average item recommendations for userX from userX's k NNs
         # Creating list of artists and noting the missing ones for UserX
         artistlist = []
         for k2,v2 in self.usersItemRatings.items():
@@ -129,7 +107,7 @@ class UserBasedFilteringRecommender:
         namesclosest = []
         kList = list(range(self.k)) 
         
-        # return sorted list of recommendations (sorted highest to lowest ratings)
+        # returning sorted list of recommendations (sorted highest to lowest ratings)
         for kvalue in kList:
             namesclosest.append(sortedlst[kvalue][0])
 
@@ -137,7 +115,7 @@ class UserBasedFilteringRecommender:
             if artist not in self.usersItemRatings[userX].keys():
                 missingartist.append(artist)
         
-        #modifying the pearson correlation
+        #modifying the pearson correlation to account for effects of negative correlation values
         pcmod = {}
         for name in namesclosest:
             pcmod[name] =(pearsinner[name]+1)/2
@@ -160,7 +138,6 @@ class UserBasedFilteringRecommender:
                     artistsum = artistsum + weights[name]*self.usersItemRatings[name][artist]
             weightedrating[artist] = round(artistsum,2)
             
-        
         #creating the final list of tuples
         finallst = []
         for finalk, finalv in weightedrating.items():
@@ -170,10 +147,6 @@ class UserBasedFilteringRecommender:
         
         sortedfinallst = sorted(finallst, key=itemgetter(1), reverse = True)
         return(sortedfinallst[0:self.m])
-        
-        # example: [('Broken Bells', 2.64), ('Vampire Weekend', 2.2), ('Deadmau5', 1.71)]
-        
-        # once you are done coding this method, delete the pass statement below
        
 
 
